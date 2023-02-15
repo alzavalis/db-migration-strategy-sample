450e07d05a15ce1bea4da1e9e6cbf65f9bcd1b1a
Alicia Zavalis <alzavalis@gmail.com>
2023-02-15 22:49:56 +0100

    Register routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Application, Request, Response } from 'express'
+import { save, migrate } from './controllers'
 import { AppResources } from './types'
 
 export default ({
@@ -8,4 +9,7 @@ export default ({
   app: Application
   resources: AppResources
 }) => {
+  app.get('/ping', (_req: Request, res: Response) => res.send('pong'))
+  app.post('/save', save(resources))
+  app.post('/internal/migrate', migrate(resources))
 }
